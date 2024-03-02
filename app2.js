eb99317f9c36b4ae2f52c5c0b92a93ae4c2339d1
Mohib <mohibullahmohim2020@gmail.com>
2024-03-02 16:42:49 +0600

    directory change and userName display

--- app2.js
+++ app2.js
@@ -1,23 +1,27 @@
-
-function loadData(){
-    const url = 'https://jsonplaceholder.typicode.com/todos/1';
-    fetch(url)
-          .then(response => response.json())
-          .then(data => console.log(data))
-}
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then(usersData => displayUser(usersData))
 
 
-function usersData(){
+// function usersData(){
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(usersData => displayUser(usersData))
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(usersData => displayUser(usersData))
 
-}
+// }
 
 function displayUser(usersData){
-    
+    const userList = document.getElementById('user-list');
     for (const singleData of usersData) {
         console.log(singleData.name);
+        const li = document.createElement('li');
+        li.style.fontSize = '22px';
+        li.style.padding = '10px';
+        li.style.margin = '10px';
+        li.style.borderRadius = '10px';
+        li.style.border = '1px solid black';
+        li.innerText = singleData.name;
+        userList.appendChild(li);
     }
 }
